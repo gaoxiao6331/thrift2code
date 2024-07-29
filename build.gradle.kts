63205f267cbd9da21c245609ab3cd6a87211bec3
gaoxiao6331 <gaoxiao6331@163.com>
2024-07-30 01:12:11 +0800

    chore: package & mirror & .idea ignore

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,11 @@ group = "gaoxiao6331"
 version = "1.0-SNAPSHOT"
 
 repositories {
+    maven { setUrl("https://maven.aliyun.com/repository/central") }
+    maven { setUrl("https://maven.aliyun.com/repository/jcenter") }
+    maven { setUrl("https://maven.aliyun.com/repository/google") }
+    maven { setUrl("https://maven.aliyun.com/repository/gradle-plugin") }
+    maven { setUrl("https://maven.aliyun.com/repository/public") }
     mavenCentral()
 }
 
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,4 @@
-package org.example
+package gaoxiao6331
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
