9a4423646c2c565bc0832d3c27418e0003c80ea2
gaoxiao6331 <gaoxiao6331@163.com>
2024-08-08 00:08:27 +0800

    feat: Literal

---
+++ src/main/kotlin/commom/Literal.kt
@@ -0,0 +1,12 @@
+package gaoxiao6331.commom
+
+enum class Literal {
+    CommentLine,
+    CommentBlock,
+    StringLiteral,
+    IntLiteral,
+    FloatLiteral,
+    HexLiteral,
+    ExponentialLiteral,
+    BooleanLiteral,
+}
