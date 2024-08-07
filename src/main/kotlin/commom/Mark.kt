d1da04a839119845e464a1fd1a356ff2eecd4422
gaoxiao6331 <gaoxiao6331@163.com>
2024-08-08 07:53:41 +0800

    feat: mark

---
+++ src/main/kotlin/commom/Mark.kt
@@ -0,0 +1,19 @@
+package gaoxiao6331.commom
+
+enum class Mark {
+    LeftParenthesisToken,
+    RightParenParenthesisToken,
+    LeftBraceToken,
+    RightBraceToken,
+    LeftBracketToken,
+    RightBracketToken,
+    CommaToken,
+    DotToken,
+    MinusToken,
+    SemicolonToken,
+    ColonToken,
+    StarToken,
+    EqualToken,
+    LessThanToken,
+    GreaterThanToken,
+}
