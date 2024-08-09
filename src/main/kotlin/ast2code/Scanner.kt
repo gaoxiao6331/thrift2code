d23a2fcb4454e613e134fac885d5e96d065a844f
gaoxiao6331 <gaoxiao6331@163.com>
2024-08-09 16:24:04 +0800

    wip: ast 4

--- src/main/kotlin/ast2code/Scanner.kt
+++ src/main/kotlin/ast2code/Scanner.kt
@@ -4,38 +4,50 @@ import gaoxiao6331.commom.exception.InternalException
 
 class Scanner {
 
-    private var line = 1
-    private var column = 1
-    private var idx = 0
+    private var pos = Position(1,1,0)
     private var source = ""
+    private var startPos = Position(1,1,0)
 
     private fun reset(source: String) {
-        this.line = 1
-        this.column = 1
+        this.pos = Position(1, 1,0)
         this.source = source
-        this.idx = 0
     }
 
-    private fun isEnd(): Boolean {
-        return this.idx >= this.source.length
+    private fun saveStartPos() {
+        this.startPos = this.pos.copy()
+    }
+
+    private fun isEnd(i: Int = this.pos.index): Boolean {
+        return i >= this.source.length
     }
 
     private fun advance(): Char {
-        val curChar = source[idx]
-        idx++
+        val curChar = source[this.pos.index]
+        this.pos.index++
         return curChar
     }
 
     private fun previous(n: Int = 1): Char {
-        val prevIdx = idx - n
+        val prevIdx = this.pos.index - n
         if (prevIdx >= 0) {
             return source[prevIdx]
         }
-        throw InternalException("current index is ${idx}, cannot get previous $n")
+        throw InternalException("current index is ${this.pos.index}, cannot get previous ${n}th char")
     }
 
+    private fun next(n: Int = 1): Char {
+        val nextIdx = this.pos.index + n
+        if (!isEnd(nextIdx)) {
+            return source[nextIdx]
+        }
+        throw InternalException("current index is ${this.pos.index}, total length is ${this.source.length}, cannot get next ${n}th char")
+    }
+
+
    fun scan(source: String) {
+       reset(source)
        while (!isEnd()) {
+           saveStartPos()
 
        }
    }
--- src/main/kotlin/ast2code/TokenData.kt
+++ src/main/kotlin/ast2code/TokenData.kt
@@ -2,7 +2,7 @@ package gaoxiao6331.ast2code
 
 import gaoxiao6331.commom.token.Token
 
-data class Position(val line: Int, val column: Int, val index: Int)
+data class Position(var line: Int, var column: Int, var index: Int)
 
 data class Location(val start: Position, val end: Position)
 
