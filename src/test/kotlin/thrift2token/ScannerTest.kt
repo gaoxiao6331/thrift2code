9c1c9a54e401b7973d4c4eb26940bb315a6c519b
gaoxiao6331 <gaoxiao6331@163.com>
2024-08-22 00:12:15 +0800

    wip: ast test 8

--- src/test/kotlin/thrift2token/ScannerTest.kt
+++ src/test/kotlin/thrift2token/ScannerTest.kt
@@ -1,2 +1,13 @@
 package thrift2token
 
+import kotlin.test.Test
+import org.junit.jupiter.api.Assertions.*
+
+class ScannerTest {
+
+    @Test
+    fun testSharedToken() {
+        assertEquals(2, 1 + 1)
+        assertEquals(3, 1 + 1)
+    }
+}
